4c8bcc43b5589504a83e9260cdcf1f7373f2cf7f
Jens <jens-kienle@web.de>
2022-08-10 16:54:09 +0200

    finished the implementation of the priority queue, tests still need to be done

--- src/datastructures/priority_queue.hpp
+++ src/datastructures/priority_queue.hpp
@@ -1,27 +1,32 @@
+#include <unordered_map>
+
 //this is the implementation of priority queue using a binary heap
 class PriorityQueue {
 private:
-  std::vector<std::pair<algen::Weight, algen::VertexId>> heap;
+  std::vector<std::pair<algen::Weight, algen::VertexId>> heap;  //the binary heap
+  std::unordered_map<algen::VertexId, long> index_map;          //maps a value to it's index in the 'heap' vector
 
-  int parent(int current) {
+  long parent(long current) {
     return (current - 1) / 2;
   }
 
-  int leftChild(int current) {
+  long leftChild(long current) {
     return ((2 * current) + 1);
   }
 
-  int rightChild(int current) {
+  long rightChild(long current) {
     return ((2 * current) + 2);
   }
 
-  void swap(int i, int j) {
+  void swap(long i, long j) {
     std::pair<algen::Weight, algen::VertexId> tmp = heap[i];
     heap[i] = heap[j];
     heap[j] = tmp;
+    index_map[heap[i].second] = i;
+    index_map[heap[j].second] = j;
   }
 
-  void shiftUp(int target) {
+  void shiftUp(long target) {
     //as long as the target isn't already root and it's parent has a smaller key, swap them
     while (target > 0 && heap[parent(target)].first > heap[target].first) {
       swap(parent(target), target);
@@ -29,8 +34,8 @@ private:
     }
   }
 
-  void shiftDown(int target) {
-    int minIndex = target;
+  void shiftDown(long target) {
+    long minIndex = target;
 
     //check if the left child of the target (if it exists) has a smaller key
     if(leftChild(target) <= heap.size() && heap[leftChild(target)].first < heap[minIndex].first) {
@@ -62,6 +67,7 @@ public:
   //pushes a new element with value 'val' and priority 'key' to the priority queue
   void push(std::pair<algen::Weight, algen::VertexId> new_element) {
     heap.push_back(new_element);
+    index_map.insert({new_element.second , heap.size() - 1});
     shiftUp(heap.size() - 1);
   }
 
@@ -78,13 +84,23 @@ public:
   }
 
   //returns the current priority of a given element with value 'val'
-  //returns algen::VERTEXID_UNDEFINED if there is no such element in the queue
+  //asserts that the given value is in the queue
   algen::Weight get_key(algen::VertexId val) {
-    //TODO
+    assert(index_map.find(val) != index_map.end());
+    return heap[index_map[val]].first;
   }
 
   //changes the priority of a given element with value 'val' to the priority 'new_key'
+  //asserts that the given value is in the queue
   void set_key(algen::VertexId val, algen::Weight new_key) {
-    //TODO
+    assert(index_map.find(val) != index_map.end());
+    long pos = index_map[val]; //insert position of value here
+    algen::Weight old_key = heap[pos].first;
+    heap[pos].first = new_key;
+    if (new_key < old_key) {
+      shiftUp(pos);
+    } else if (new_key > old_key) {
+      shiftDown(pos);
+    }
   }
 };
